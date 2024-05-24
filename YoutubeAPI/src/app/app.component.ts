edd11ac6fbe836abcac5efe6e8e0c976f751702a
brooklynTW <64011655@kmitl.ac.th>
2024-05-24 16:12:48 +0700

    Transition failed
    - made the h3 video title to be clickable

--- YoutubeAPI/src/app/app.component.ts
+++ YoutubeAPI/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.5s ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('0.5s ease-in', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'YoutubeAPI';
 }
