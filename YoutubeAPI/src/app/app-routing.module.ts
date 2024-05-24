5c10995f58db13b92b84c9d7c9dd307afd29dc56
brooklynTW <64011655@kmitl.ac.th>
2024-05-24 14:56:34 +0700

    update navbar

--- YoutubeAPI/src/app/app-routing.module.ts
+++ YoutubeAPI/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TopVideosComponent } from './top-videos/top-videos.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/top-videos', pathMatch: 'full' },
   { path: 'top-videos', component: TopVideosComponent },
+  { path: 'home', component: HomeComponent },
   // Add more routes as needed
 ];
 
--- YoutubeAPI/src/app/app.module.ts
+++ YoutubeAPI/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopVideosComponent } from './top-videos/top-videos.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopVideosComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
